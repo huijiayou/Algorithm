4ecf4496dd43993761f38a8a48bde584187bf06a
Lisa Ren <82609299+huijiayou@users.noreply.github.com>
2022-06-23 11:36:56 -0400

    Time: 259 ms (18.72%), Space: 62.7 MB (28.83%) - LeetHub

---
+++ 454-4sum-ii/454-4sum-ii.java
@@ -0,0 +1,27 @@
+/*
+    group 2 arrays together to get k: sum - v: count
+    and make it into a 2sum question
+    TC: O(n^2)
+    SC: O(n)
+*/
+
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n1 : nums1) {
+            for (int n2 : nums2) {
+                map.put(n1 + n2, map.getOrDefault(n1 + n2, 0) + 1);
+            }
+        }
+        int res = 0;
+        for (int n3 : nums3) {
+            for (int n4 : nums4) {
+                int remains = 0 - (n3 + n4);
+                if (map.containsKey(remains)) {
+                    res += map.get(remains);
+                }
+            }
+        }
+        return res;
+    }
+}
