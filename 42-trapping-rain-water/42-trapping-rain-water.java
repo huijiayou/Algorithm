e86685bb5fb369c0bec1f0073f37b6233e5b50ed
Lisa Ren <82609299+huijiayou@users.noreply.github.com>
2022-06-27 16:06:56 -0400

    Time: 7 ms (14.66%), Space: 50 MB (5.33%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,41 +1,15 @@
-/*
-    from left and right to middle
-    which side is lower, move it's pointer
-    
-    4   2   0   3   2   5
-            i
-                    j
-    left        
-                        right
-        2   4   1   2        
-   
-    0    1   0   2   1   0   1   3   2   1   2   1
-                                left   
-                                     right
-                        i
-                                            j
-             1      1   2   1           1
-*/
-
 class Solution {
     public int trap(int[] height) {
-        if (height.length <= 2) {
-            return 0;
-        }
-        int left = height[0];
-        int right = height[height.length - 1];
-        int i = 1;
-        int j = height.length - 2;
-        int total = 0;
-        while (i <= j) {
-            if (left <= right) {
-                left = Math.max(left, height[i]);
-                total += left - height[i++];
-            } else {
-                right = Math.max(right, height[j]);
-                total += right - height[j--];
+        int res = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < height.length; i++) {
+            while (!stack.isEmpty() && height[i] > height[stack.peekFirst()]) {
+                int bottom = stack.pollFirst();
+                if (stack.isEmpty()) break;
+                res += (Math.min(height[i], height[stack.peekFirst()]) - height[bottom]) * (i - stack.peekFirst() - 1);
             }
+            stack.offerFirst(i);
         }
-        return total;
+        return res;
     }
 }
