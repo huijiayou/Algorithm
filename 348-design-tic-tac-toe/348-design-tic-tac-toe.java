87ee1cc8088b4a33363a072afd86d34b2ec846b3
Lisa Ren <82609299+huijiayou@users.noreply.github.com>
2022-07-31 17:34:05 -0400

    Time: 19 ms (6.80%), Space: 48.7 MB (5.89%) - LeetHub

--- 348-design-tic-tac-toe/348-design-tic-tac-toe.java
+++ 348-design-tic-tac-toe/348-design-tic-tac-toe.java
@@ -23,10 +23,8 @@ class TicTacToe {
         for (int i = 0; i < n; i++) {
             if (board[i][col] == piece) total[0]++;
             if (board[row][i] == piece) total[1]++;
-            if (row + i < n && col + i < n && board[row + i][col + i] == piece) total[2]++;
-            if (i > 0 && row - i >= 0 && col - i >= 0 && board[row - i][col - i] == piece) total[2]++;
-            if (row + i < n && col - i >= 0 && board[row + i][col - i] == piece) total[3]++;
-            if (i > 0 && row - i >= 0 && col + i < n && board[row - i][col + i] == piece) total[3]++;
+            if (board[i][i] == piece) total[2]++;
+            if (board[i][n - 1 - i] == piece) total[3]++;
             if (total[0] == n || total[1] == n || total[2] == n || total[3] == n) {
                 return true;
             }
