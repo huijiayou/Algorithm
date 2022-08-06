8136a54d5720f1d2bab260407d2ab71093ea7147
Lisa Ren <82609299+huijiayou@users.noreply.github.com>
2022-08-06 15:23:03 -0400

    Time: 365 ms (29.02%), Space: 132.9 MB (39.51%) - LeetHub

--- 2102-sequentially-ordinal-rank-tracker/2102-sequentially-ordinal-rank-tracker.java
+++ 2102-sequentially-ordinal-rank-tracker/2102-sequentially-ordinal-rank-tracker.java
@@ -1,30 +1,38 @@
 class SORTracker {
-    static class Rank implements Comparable<Rank>{
+    static class Rank {
         String name;
         int score;
         public Rank (String name, int score) {
             this.name = name;
             this.score = score;
         }
-        @Override
-        public int compareTo(Rank r1) {
-            if (this.score == r1.score) return r1.name.compareTo(this.name);
-            return this.score < r1.score ? -1 : 1;
-        }
     }
     private PriorityQueue<Rank> minheap;
     private PriorityQueue<Rank> maxheap;
-    
+
     public SORTracker() {
-        minheap = new PriorityQueue<>();
-        maxheap = new PriorityQueue<>(Comparator.reverseOrder());
+        minheap = new PriorityQueue<>(new Comparator<Rank>(){
+            @Override
+            public int compare(Rank r1, Rank r2) {
+                int res = r2.name.compareTo(r1.name);
+                if (r1.score == r2.score) return res;
+                return r1.score < r2.score ? -1 : 1;
+            }
+        });
+        maxheap = new PriorityQueue<>(new Comparator<Rank>(){
+            @Override
+            public int compare(Rank r1, Rank r2) {
+                if (r1.score == r2.score) return r1.name.compareTo(r2.name);
+                return r1.score < r2.score ? 1 : -1;
+            }
+        });
     }
-    
+
     public void add(String name, int score) {
         minheap.offer(new Rank(name, score));
         maxheap.offer(minheap.poll());
     }
-    
+
     public String get() {
         minheap.offer(maxheap.poll());
         return minheap.peek().name;
