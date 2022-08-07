fb31f1fb658298f070cc32ad5639828f7d2f2f48
Lisa Ren <82609299+huijiayou@users.noreply.github.com>
2022-08-07 17:49:53 -0400

    Time: 29 ms (89.52%), Space: 67.5 MB (63.43%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.java
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.java
@@ -1,31 +1,17 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int n = arr.length;
-        int modulo = 1000000007;
-        int[] left = new int[n];
-        Arrays.fill(left, -1);
-        Deque<Integer> min = new ArrayDeque<>();
-        for (int i = 0; i < arr.length; i++) {
-            while (!min.isEmpty() && arr[min.peekLast()] > arr[i]) {
-                min.pollLast();
-            }
-            left[i] = min.isEmpty() ? -1 : min.peekLast();
-            min.offerLast(i);
-        }
-        min = new ArrayDeque<>();
-        int[] right = new int[n];
-        Arrays.fill(right, n);
-        for (int i = arr.length - 1; i >= 0; i--) {
-            while (!min.isEmpty() && arr[min.peekLast()] >= arr[i]) {
-                min.pollLast();
-            }
-            right[i] = min.isEmpty() ? n : min.peekLast();
-            min.offerLast(i);
-        }
         long sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += (arr[i] * (long)(i - left[i]) * (long)(right[i] - i));
-            sum %= modulo;
+        Deque<Integer> min = new ArrayDeque<>();
+        int modulo = 1000000007;
+        int n = arr.length;
+        for (int i = 0; i <= n; i++) {
+            while (!min.isEmpty() && (i == n || arr[min.peekLast()] > arr[i])) {
+                int c = min.pollLast();
+                int l = min.isEmpty() ? -1 : min.peekLast();
+                sum += arr[c] * (long)(c - l) * (long)(i - c);
+                sum %= modulo;
+            }
+            min.offerLast(i);
         }
         return (int)sum;
     }
