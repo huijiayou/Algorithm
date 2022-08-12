ed46b486c7802ac4e7daceb9223719c93bfda594
Lisa Ren <82609299+huijiayou@users.noreply.github.com>
2022-08-11 23:12:40 -0400

    Time: 46 ms (71.59%), Space: 50.4 MB (44.12%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        /*
+            babad
+            
+            abba
+            using each letter as the center and expand
+            - aba
+            - bb
+        */
+        int bestLen = 0;
+        String res = "";
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            for (int x = 0; i - x >= 0 && i + x < n; x++) {
+                if (s.charAt(i - x) != s.charAt(i + x)) {
+                    break;
+                }
+                int len = 2 * x + 1;
+                if (len > bestLen) {
+                    bestLen = len;
+                    res = s.substring(i - x, i - x + bestLen);
+                }
+            }
+        }
+        for (int i = 0; i < n - 1; i++) {
+            for (int x = 1; i - x + 1 >= 0 && i + x < n; x++) {
+                if (s.charAt(i - x + 1) != s.charAt(i + x)) {
+                    break;
+                }
+                int len = 2 * x;
+                if (len > bestLen) {
+                    bestLen = len;
+                    res = s.substring(i - x + 1, i - x + 1 + bestLen);
+                }
+            }
+        }
+        return res;
+    }
+}
