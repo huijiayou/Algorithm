c6c42a0e1cd23d34742a2d61c825ef5c92a57fb6
Lisa Ren <82609299+huijiayou@users.noreply.github.com>
2022-08-13 11:52:49 -0400

    Time: 6 ms (71.68%), Space: 42.2 MB (57.72%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -10,7 +10,7 @@ class Solution {
                 res++;
             }
         }
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n; i++) {
             for (int x = 1; i - x + 1 >= 0 && i + x < n; x++) {
                 if (s.charAt(i - x + 1) != s.charAt(i + x)) {
                     break;
