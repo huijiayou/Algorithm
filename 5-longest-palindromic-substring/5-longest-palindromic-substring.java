6ecf00b97545e45c7a53ab57e80cc9f3f58b1a62
Lisa Ren <82609299+huijiayou@users.noreply.github.com>
2022-08-13 11:53:08 -0400

    Time: 28 ms (94.30%), Space: 42.7 MB (84.79%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -23,7 +23,7 @@ class Solution {
                 }
             }
         }
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n; i++) {
             for (int x = 1; i - x + 1 >= 0 && i + x < n; x++) {
                 if (s.charAt(i - x + 1) != s.charAt(i + x)) {
                     break;
