8646dc36de7d72b2bf3183ffb0250ec7b27fe126
Lisa Ren <82609299+huijiayou@users.noreply.github.com>
2022-08-21 12:07:18 -0400

    Time: 0 ms (100.00%), Space: 42.2 MB (48.41%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -1,41 +1,40 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        if (matrix.length == 0 || matrix[0].length == 0) {
-            return new ArrayList<>();
-        }
         List<Integer> res = new ArrayList<>();
-        dfs(matrix, res, 0, matrix.length, matrix[0].length);
+        int row = matrix.length;
+        int col = matrix[0].length;
+        dfs(matrix, row, col, 0, res);
         return res;
     }
-    private void dfs(int[][] matrix, List<Integer> res, int offset, int rows, int cols) {
-        if (rows == 0 || cols == 0) {
+    private void dfs(int[][] matrix, int rowLen, int colLen, int offset, List<Integer> res) {
+        if (rowLen == 0 || colLen == 0) {
             return;
-        } else if (rows <= 1 && cols > 1) {
-            for (int i = 0; i < cols; i++) {
+        } else if (rowLen == 1 && colLen == 1) {
+            res.add(matrix[offset][offset]);
+            return;
+        } else if (rowLen == 1 && colLen > 1) {
+            for (int i = 0; i < colLen; i++) {
                 res.add(matrix[offset][offset + i]);
             }
             return;
-        } else if (rows > 1 && cols <= 1) {
-            for (int i = 0; i < rows; i++) {
+        } else if (colLen == 1 && rowLen > 1) {
+            for (int i = 0; i < rowLen; i++) {
                 res.add(matrix[offset + i][offset]);
             }
             return;
-        } else if(rows == 1 && cols == 1) {
-            res.add(matrix[offset][offset]);
-            return;
         }
-        for (int i = 0; i < cols - 1; i++) {
+        for (int i = 0; i < colLen - 1; i++) {
             res.add(matrix[offset][offset + i]);
         }
-        for (int i = 0; i < rows - 1; i++) {
-            res.add(matrix[offset + i][offset + cols - 1]);
+        for (int i = 0; i < rowLen - 1; i++) {
+            res.add(matrix[offset + i][offset + colLen - 1]);
         }
-        for (int i = cols - 1; i > 0; i--) {
-            res.add(matrix[offset + rows - 1][offset + i]);
+        for (int i = colLen - 1; i > 0; i--) {
+            res.add(matrix[offset + rowLen - 1][offset + i]);
         }
-        for (int i = rows - 1; i > 0; i--) {
+        for (int i = rowLen - 1; i > 0; i--) {
             res.add(matrix[offset + i][offset]);
         }
-        dfs(matrix, res, offset + 1, rows - 2, cols - 2);
+        dfs(matrix, rowLen - 2, colLen - 2, offset + 1, res);
     }
 }
