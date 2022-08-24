f4c7655fc638db11276eb705200aa20d23e4b312
Lisa Ren <82609299+huijiayou@users.noreply.github.com>
2022-08-24 17:27:07 -0400

    Time: 7 ms (99.27%), Space: 47.3 MB (91.59%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,15 +1,6 @@
-/*
-    1. sort the 2-d array based on their start value
-    2. for each inner array:
-        - if end is larger than next start, but smaller than next end, update end
-        - if end if larger than next end, skip next
-        - else, go to next
-*/
-
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<int[]> list = new ArrayList<>();
-        Arrays.sort(intervals, new Comparator<int[]>() {
+        Arrays.sort(intervals, new Comparator<int[]> () {
             @Override
             public int compare(int[] arr1, int[] arr2) {
                 if (arr1[0] == arr2[0]) {
@@ -18,21 +9,21 @@ class Solution {
                 return arr1[0] < arr2[0] ? -1 : 1;
             }
         });
-        
-        for (int i = 0; i < intervals.length; i++) {
-            list.add(intervals[i]);
-            while (i + 1 < intervals.length && list.get(list.size() - 1)[1] >= intervals[i + 1][0]) {
-                int end = list.get(list.size() - 1)[1];
-                if (end >= intervals[i + 1][0] && end < intervals[i + 1][1] ) {
-                    list.get(list.size() - 1)[1] = intervals[i + 1][1];
-                }
+        int n = intervals.length;
+        List<int[]> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int[] cur = intervals[i];
+            while (i + 1 < n && cur[1] >= intervals[i + 1][0]) {
+                cur[0] = Math.min(cur[0], intervals[i + 1][0]);
+                cur[1] = Math.max(cur[1], intervals[i + 1][1]);
                 i++;
             }
+            list.add(cur);
         }
-        int[][] res = new int[list.size()][];
+        int[][] result = new int[list.size()][];
         for (int i = 0; i < list.size(); i++) {
-            res[i] = list.get(i);
+            result[i] = list.get(i);
         }
-        return res;
+        return result;
     }
 }
