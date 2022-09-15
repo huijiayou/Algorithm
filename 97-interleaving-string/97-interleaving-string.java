fed09906a1939dec1825ef3fc0da6a4bf33fd947
Lisa Ren <82609299+huijiayou@users.noreply.github.com>
2022-09-15 19:42:28 -0400

    Time: 9 ms (31.00%), Space: 42 MB (79.21%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int n = s1.length();
+        int m = s2.length();
+        if (n + m != s3.length()) return false;
+        boolean[][] dp = new boolean[n + 1][m + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= m; i++) {
+            if (s2.charAt(i - 1) == s3.charAt(i - 1) && dp[0][i - 1]) {
+                dp[0][i] = true;
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            if (s1.charAt(i - 1) == s3.charAt(i - 1) && dp[i - 1][0]) {
+                dp[i][0] = true;
+            }
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (s1.charAt(i - 1) == s3.charAt(i + j - 1) && dp[i - 1][j] || s2.charAt(j - 1) == s3.charAt(i + j - 1) && dp[i][j - 1]) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
