719ba1477e5a0ad2f78899bd19fec4716b593556
Lisa Ren <82609299+huijiayou@users.noreply.github.com>
2022-09-17 13:54:03 -0400

    Time: 1 ms (99.46%), Space: 42.8 MB (22.08%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,31 +1,23 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        char[] char1 = s1.toCharArray();
-        char[] char2 = s2.toCharArray();
-        char[] char3 = s3.toCharArray();
         int n = s1.length();
         int m = s2.length();
-        int l = s3.length();
-        if (n + m != l) return false;
+        if (n + m != s3.length()) return false;
         boolean[][] isvalid = new boolean[n + 1][m + 1];
-        return dfs(char1, char2, char3, 0, 0, 0, isvalid);
+        return dfs(s1, s2, s3, 0, 0, 0, isvalid);
     }
-    private boolean dfs(char[] char1, char[] char2, char[] char3, int i, int j, int k, boolean[][] isvalid) {
-        if(isvalid[i][j]) return false;
-        if(k == char3.length) return true;
-        /*
-        boolean left = false;
-        if (i < char1.length && char1[i] == char3[k] && dfs(char1, char2, char3, i + 1, j, k + 1, isvalid)) {
-            left = true;
+    private boolean dfs(String s1, String s2, String s3, int i, int j, int k, boolean[][] isvalid) {
+        if (isvalid[i][j]) return false;
+        if (k == s3.length() && i == s1.length() && j == s2.length()) return true;
+        boolean first = i < s1.length() && s1.charAt(i) == s3.charAt(k) && dfs(s1, s2, s3, i + 1, j, k + 1, isvalid);
+        if (first) {
+            return true;
         }
-        boolean right = false;
-        if (j < char2.length && char2[j] == char3[k] && dfs(char1, char2, char3, i, j + 1, k + 1, isvalid)) {
-            right = true;
+        boolean second = j < s2.length() && s2.charAt(j) == s3.charAt(k) && dfs(s1, s2, s3, i, j + 1, k + 1, isvalid);
+        if (second) {
+            return true;
         }
-        */
-        boolean valid = i < char1.length && char1[i] == char3[k] && dfs(char1, char2, char3, i + 1, j, k + 1, isvalid) ||
-            j < char2.length && char2[j] == char3[k] && dfs(char1, char2, char3, i, j + 1, k + 1, isvalid);
-        if(!valid) isvalid[i][j] = true;
-        return valid;
+        isvalid[i][j] = true;
+        return false;
     }
 }
