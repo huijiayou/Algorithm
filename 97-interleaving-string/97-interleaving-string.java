a7eccd3f7ce3c6e1e7c62f4abca2fa7b8150d80a
Lisa Ren <82609299+huijiayou@users.noreply.github.com>
2022-09-18 12:48:42 -0400

    Time: 1 ms (99.46%), Space: 42.1 MB (75.51%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -3,21 +3,23 @@ class Solution {
         int n = s1.length();
         int m = s2.length();
         if (n + m != s3.length()) return false;
-        boolean[][] isvalid = new boolean[n + 1][m + 1];
-        return dfs(s1, s2, s3, 0, 0, 0, isvalid);
+        boolean[][] invalid = new boolean[n + 1][m + 1];
+        return dfs(s1, s2, s3, 0, 0, 0, invalid);
     }
-    private boolean dfs(String s1, String s2, String s3, int i, int j, int k, boolean[][] isvalid) {
-        if (isvalid[i][j]) return false;
-        if (k == s3.length() && i == s1.length() && j == s2.length()) return true;
-        boolean first = i < s1.length() && s1.charAt(i) == s3.charAt(k) && dfs(s1, s2, s3, i + 1, j, k + 1, isvalid);
-        if (first) {
-            return true;
+    private boolean dfs(String s1, String s2, String s3, int i, int j, int k, boolean[][] invalid) {
+        if (invalid[i][j]) return false;
+        if (k == s3.length()) return true;
+        if (i < s1.length() && s1.charAt(i) == s3.charAt(k)) {
+            if (dfs(s1, s2, s3, i + 1, j, k + 1, invalid)) {
+                return true;
+            }
         }
-        boolean second = j < s2.length() && s2.charAt(j) == s3.charAt(k) && dfs(s1, s2, s3, i, j + 1, k + 1, isvalid);
-        if (second) {
-            return true;
+        if (j < s2.length() && s2.charAt(j) == s3.charAt(k)) {
+            if (dfs(s1, s2, s3, i, j + 1, k + 1, invalid)) {
+                return true;
+            }
         }
-        isvalid[i][j] = true;
+        invalid[i][j] = true;
         return false;
     }
 }
