76571a3d46612110e3e1d48cd79c00650b82e1e3
Lisa Ren <82609299+huijiayou@users.noreply.github.com>
2022-09-19 11:35:24 -0400

    Time: 3 ms (43.27%), Space: 42.7 MB (30.19%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,19 +1,19 @@
 class Solution {
     public int numDecodings(String s) {
         int n = s.length();
+        if (s.charAt(0) == '0') return 0;
         int[] dp = new int[n + 1];
-        return dfs(s, 0, dp);
-    }
-    private int dfs(String s, int i, int[] dp) {
-        if (i == s.length()) return 1;
-        if (s.charAt(i) == '0') return 0;
-        if (dp[i] > 0) return dp[i];
-        int one = dfs(s, i + 1, dp);
-        int two = 0;
-        if (i + 1 < s.length() && Integer.valueOf(s.substring(i, i + 2)) <= 26) {
-            two = dfs(s, i + 2, dp);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < dp.length; i++) {
+            if (s.charAt(i - 1) != '0') {
+                dp[i] += dp[i - 1];
+            }
+            int temp = Integer.valueOf(s.substring(i - 2, i));
+            if (temp >= 10 && temp <= 26) {
+                dp[i] += dp[i - 2];
+            }
         }
-        dp[i] = one + two;
-        return dp[i];
+        return dp[n];
     }
 }
