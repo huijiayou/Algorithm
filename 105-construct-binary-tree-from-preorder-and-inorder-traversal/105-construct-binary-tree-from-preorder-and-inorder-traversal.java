0368108cbca35e43e3d08794f1e39e5e4359bbd1
Lisa Ren <82609299+huijiayou@users.noreply.github.com>
2022-09-25 16:23:27 -0400

    Time: 3 ms (89.05%), Space: 45.3 MB (13.74%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,22 +15,19 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        int n = preorder.length;
-        Map<Integer, Integer> inOrderMap = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            inOrderMap.put(inorder[i], i);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
         }
-        return buildTree(preorder, inOrderMap, 0, n - 1, 0, n - 1);
+        return dfs(preorder, map, 0, inorder.length - 1, 0, inorder.length - 1);
     }
-    private TreeNode buildTree(int[] preorder, Map<Integer, Integer> inOrderMap, int leftPre, int rightPre, int leftIn, int rightIn) {
-        if (leftPre > rightPre) {
-            return null;
-        }
+    private TreeNode dfs(int[] preorder, Map<Integer, Integer> map, int leftPre, int rightPre, int leftIn, int rightIn) {
+        if (leftPre > rightPre) return null;
         TreeNode root = new TreeNode(preorder[leftPre]);
-        int rootIndexIn = inOrderMap.get(preorder[leftPre]);
-        int leftCount = rootIndexIn - leftIn;
-        root.left = buildTree(preorder, inOrderMap, leftPre + 1, leftPre + leftCount, leftIn, rootIndexIn - 1);
-        root.right = buildTree(preorder, inOrderMap, leftPre + leftCount + 1, rightPre, rootIndexIn + 1, rightIn);
+        int indexIn = map.get(preorder[leftPre]);
+        int leftCount = indexIn - leftIn;
+        root.left = dfs(preorder, map, leftPre + 1, leftPre + leftCount, leftIn, indexIn - 1);
+        root.right = dfs(preorder, map, leftPre + leftCount + 1, rightPre, indexIn + 1, rightIn);
         return root;
     }
 }
